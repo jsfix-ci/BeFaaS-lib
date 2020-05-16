885ccca19433de6c858f4f827916a885dba5879e
Christoph Witzko <github@christophwitzko.com>
2020-05-16 20:36:14 +0200

    fix(helper): google detection

--- __tests__/helper.js
+++ __tests__/helper.js
@@ -4,3 +4,7 @@ const helper = require('../helper')
 test('isLambda', () => {
   expect(helper.isLambda).toBe(false)
 })
+
+test('iGoogle', () => {
+  expect(helper.isGoogle).toBe(false)
+})
--- helper.js
+++ helper.js
@@ -1,4 +1,4 @@
 module.exports = {
   isLambda: !!process.env.AWS_LAMBDA_FUNCTION_NAME,
-  isGoogle: !!process.env.GCP_PROJECT
+  isGoogle: !!process.env.K_SERVICE && !!process.env.K_REVISION
 }
