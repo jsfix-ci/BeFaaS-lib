0d6e0a9832ad0bc65e6b474d1ab405f4eed2696f
Christoph Witzko <github@christophwitzko.com>
2020-05-20 14:13:44 +0200

    feat(call): automatically detected provider
    BREAKING CHANGE: lib.call function signature change

--- call.js
+++ call.js
@@ -1,11 +1,15 @@
+const _ = require('lodash')
 const fetch = require('node-fetch')
+
+const helper = require('./helper')
 
 const endpoints = {
   aws: process.env.AWS_LAMBDA_ENDPOINT,
   google: process.env.GOOGLE_CLOUDFUNCTION_ENDPOINT
 }
 
-module.exports = async (provider, fn, payload) => {
+module.exports = async (fn, payload) => {
+  const provider = _.get(helper.experiment, `program.functions.${fn}.provider`)
   if (!endpoints[provider]) throw new Error('unknown provider')
   const res = await fetch(`${endpoints[provider]}/${fn}/call`, {
     method: 'post',
--- helper.js
+++ helper.js
@@ -1,8 +1,16 @@
+const path = require('path')
+
 const helper = {
   isLambda: !!process.env.AWS_LAMBDA_FUNCTION_NAME,
   isGoogle: !!process.env.K_SERVICE && !!process.env.K_REVISION,
   isAzure: !!process.env.IS_AZURE_FUNCTION_APP
 }
+
+let experiment = {}
+
+try {
+  experiment = require(path.join(process.cwd(), 'experiment.json'))
+} catch (e) {}
 
 module.exports = {
   prefix: () => {
@@ -10,5 +18,6 @@ module.exports = {
     if (helper.isAzure) return '/api/:fn'
     return null
   },
+  experiment,
   ...helper
 }
