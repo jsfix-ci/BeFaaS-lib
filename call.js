42b6add32fc24f0f6a8941eb437eab9d046dcca8
Martin Grambow <grambow@tu-berlin.de>
2022-04-25 17:28:06 +0200

    feat: call publisher endpoint(s)

--- call.js
+++ call.js
@@ -14,18 +14,49 @@ const endpoints = {
   openwhisk: process.env.OPENWHISK_ENDPOINT
 }
 
+const publisherEndpoints = {
+  aws: process.env.PUBLISHER_AWS_ENDPOINT
+}
+
 module.exports = async (fn, contextId, xPair, payload) => {
+
+  console.log("fn is: " + fn)
   if (!_.isObject(payload)) throw new Error('payload is not an object')
-  const provider = _.get(experiment, `program.functions.${fn}.provider`)
-  if (!endpoints[provider]) throw new Error('unknown provider')
-  const res = await fetch(`${endpoints[provider]}/${fn}/call`, {
-    method: 'post',
-    body: JSON.stringify(payload || {}),
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Context': contextId,
-      'X-Pair': xPair
-    }
-  })
-  return res.json()
+  
+  var provider = ""
+  if (fn === "publisher") {
+    console.log("Will call publisher...")
+    functionName = payload.fun
+    provider = _.get(experiment, `program.functions.${functionName}.provider`)
+    console.log("provider is " + provider)
+	if (!publisherEndpoints[provider]) throw new Error('unknown publisher provider')
+  } else {
+    provider = _.get(experiment, `program.functions.${fn}.provider`)
+    console.log("provider is " + provider)
+    if (!endpoints[provider]) throw new Error('unknown provider')
+  } 
+  
+  if (fn === "publisher") {
+    const res = await fetch(`${publisherEndpoints[provider]}/call`, {
+      method: 'post',
+      body: JSON.stringify(payload || {}),
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Context': contextId,
+        'X-Pair': xPair
+      }
+    })
+    return res.json()
+  } else {	  
+    const res = await fetch(`${endpoints[provider]}/${fn}/call`, {
+      method: 'post',
+      body: JSON.stringify(payload || {}),
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Context': contextId,
+        'X-Pair': xPair
+      }
+    })
+    return res.json()
+  }
 }
